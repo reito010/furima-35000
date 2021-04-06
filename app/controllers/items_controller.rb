69d4dbcbfde0cce77244f8f7200aded12fe0f5b1
reito010 <79955877+reito010@users.noreply.github.com>
2021-04-06 16:46:46 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
+  
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -16,7 +17,7 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render action: :new
-   end
+    end
   end
 
   def show  
@@ -33,23 +34,26 @@ class ItemsController < ApplicationController
     end
   end
 
-  #def destroy
-  #end
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
 
  
-private
+ private
 
- def item_params
-  params.require(:item).permit(:name,:image,:text,:category_id,:status_id,:area_id,:cost_id,:delivery_id,:price).merge(user_id: current_user.id)
- end
+  def item_params
+   params.require(:item).permit(:name,:image,:text,:category_id,:status_id,:area_id,:cost_id,:delivery_id,:price).merge(user_id: current_user.id)
+  end
 
- def set_item
-  @item = Item.find(params[:id])
-end
+  def set_item
+   @item = Item.find(params[:id])
+  end
 
- def move_to_index
-  unless current_user == @item.user
+  def move_to_index
+   unless current_user == @item.user
     redirect_to action: :index
+   end
   end
 end
