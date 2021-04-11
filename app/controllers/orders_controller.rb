e73f545a32284d3e187c5c7c68529947f6ce9c2f
reito010 <79955877+reito010@users.noreply.github.com>
2021-04-11 17:58:09 +0900

    変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
+  before_action :set_order, only: [:index, :create]
   before_action :move_to_index, only: [:create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item 
@@ -28,6 +28,11 @@ class OrdersController < ApplicationController
      redirect_to action: :index
     end
   end
+
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
+
 
   def order_params
     params.require(:order_address).permit(:postal_code, :area_id, :city, :street, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
